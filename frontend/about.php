90898a6d53a91485027aadc260ee12a83a788651
jimmyyen <ichthyoid95@gmail.com>
2020-07-07 15:52:03 +0800

    Collection Display

--- frontend/about.php
+++ frontend/about.php
@@ -1,8 +1,8 @@
-<div class="about">
-    <div id="infoPic" class="infoPic"></div>
-    <h1 id="nameh1" class="mb-30 tcent">Hi, i'm <span id="name" class="name">Jimmy</span>!</h1>
-    <h3 class="tcent">Web Designer & Developer</h3>
-    <h5 class="mb-30 tright">Frontend / Backend</h5>
-    <hr class="mb-30 tcent">
-    <p>Welcome to my personal website.</p>
-  </div>
+<div class="about d-flex j-center a-center f-direction-c">
+  <div id="infoPic" class="infoPic"></div>
+  <h1 id="nameh1" class="mb-30 tcent">Hi, i'm <span id="name" class="name">Jimmy</span>!</h1>
+  <h3 class="tcent">Web Designer & Developer</h3>
+  <h5 class="mb-30 t-right">Frontend / Backend</h5>
+  <hr class="mb-30">
+  <p>Welcome to my personal website.</p>
+</div>
--- frontend/collection.php
+++ frontend/collection.php
@@ -3,6 +3,23 @@
   $sql = "SELECT * FROM collection";
   $Ps = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
   ?>
+
   <div class="collection">
-      <h1 class="t-center">Collection</h1>
+    <h1 class="t-center">Collection</h1>
+    <div class="collectionDiv d-flex wrap">
+      <?php
+      foreach ($Ps as $P) {
+      ?>
+        <div class="collections relative">
+          <img src="images/<?= $P['img'] ?>" alt="">
+          <span><?= $P['title'] ?></span>
+          <span><?= $P['url'] ?></span>
+          <span><?= $P['note1'] ?></span>
+          <span><?= $P['note2'] ?></span>
+          <span><?= $P['note3'] ?></span>
+        </div>
+      <?php
+      }
+      ?>
+    </div>
   </div>
