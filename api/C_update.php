47a901c2339e68dd4bc34d0e88cd0a9eabb9a4c8
jimmyyen <ichthyoid95@gmail.com>
2020-07-08 10:27:45 +0800

    作品集圖片上傳判斷

--- api/C_update.php
+++ api/C_update.php
@@ -6,8 +6,13 @@ $url =  $_POST['url'];
 $note1 =  $_POST['note1'];
 $note2 =  $_POST['note2'];
 $note3 =  $_POST['note3'];
-$filename = $_FILES['image']['name'];
-
-move_uploaded_file($_FILES['image']['tmp_name'], "../images/" . $_FILES['image']['name']);
+if (!empty($_FILES)) {
+  $filename = $_FILES['image']['name'];
+  move_uploaded_file($_FILES['image']['tmp_name'], "../images/" . $_FILES['image']['name']);
+} else {
+  $sql = "select * from collection where `id` = '$id'";
+  $collection = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+  $filename = $collection['img'];
+}
 $sql = "UPDATE collection SET `img`='$filename',`title`='$title',`url`='$url',`note1`='$note1',`note2`='$note2',`note3`='$note3' WHERE `id`= '$id'";
 $pdo->exec($sql);
