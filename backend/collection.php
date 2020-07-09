05b1646bb7afa0255425306cff1700646ecd5b1a
jimmyyen <ichthyoid95@gmail.com>
2020-07-09 15:36:10 +0800

    Backend-Done

--- backend/collection.php
+++ backend/collection.php
@@ -1,3 +1,22 @@
+<style>
+  .addP form {
+    margin: auto;
+  }
+
+  .uploadForm input,
+  .uploadForm div {
+    padding: .3rem;
+    margin: .5rem;
+  }
+  input[type="text"],
+  select{
+    padding:5px;
+    margin: 2px;
+  }
+  input[type="file"]{
+    width: 11rem;
+    }
+</style>
 <?php
 include_once "./base.php";
 $sql = "SELECT * FROM collection";
@@ -21,8 +40,6 @@ $Ps = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
         <th>URL</th>
         <th>Note</th>
         <th>Edit</th>
-        <th>Display</th>
-        <th>Delete</th>
       </tr>
       <?php
       foreach ($Ps as $P) {
@@ -66,11 +83,9 @@ $Ps = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
               <option value="PHP">PHP</option>
             </select><br>
           </td>
-          <td><i class="far fa-save editC" onclick="updateC(<?= $P['id'] ?>)"></i></td>
           <td>
-              <input id="shCheck<?= $P['id'] ?>" type="checkbox" name="sh[]" onclick="showC(<?= $P['id'] ?>)" <?= $checked ?>>
-          </td>
-          <td>
+            <input id="shCheck<?= $P['id'] ?>" type="checkbox" name="sh[]" onclick="showC(<?= $P['id'] ?>)" <?= $checked ?>>
+            <i class="far fa-save editC" onclick="updateC(<?= $P['id'] ?>)"></i>
             <div id="deleteP<?= $P['id'] ?>" class="deleteP" onclick="deleteC(<?= $P['id'] ?>)"><i class="fas fa-trash-alt"></i></div>
           </td>
         </tr>
@@ -79,45 +94,50 @@ $Ps = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
       ?>
     </table>
 
-
   </div>
-  <div id="addP" class="addP d-flex j-center a-center">
-    <form action="api/C_add.php" method="post" enctype="multipart/form-data" id="uploadForm">
+  <div id="addP" class="addP d-flex">
+    <form action="api/C_add.php" method="post" enctype="multipart/form-data" id="uploadForm" class="uploadForm">
       <label for="titleP">Title:</label>
       <input type="text" name="titleP" required><br>
       <label for="fileP">Image:</label>
       <input type="file" name="fileP" required><br>
       <label for="urlP">Url:</label>
       <input type="text" name="urlP" required><br>
+      <div>
       <label>SKill Used-1:</label>
-      <select name="noteP1">
-        <option value="HTML5">HTML5</option>
-        <option value="CSS3">CSS3</option>
-        <option value="JavaScript">JavaScript</option>
-        <option value="jQuery">jQuery</option>
-        <option value="Ajax">Ajax</option>
-        <option value="PHP">PHP</option>
-      </select><br>
-      <label>SKill Used-2:</label>
-      <select name="noteP2">
-        <option value="0">None</option>
-        <option value="HTML5">HTML5</option>
-        <option value="CSS3">CSS3</option>
-        <option value="JavaScript">JavaScript</option>
-        <option value="jQuery">jQuery</option>
-        <option value="Ajax">Ajax</option>
-        <option value="PHP">PHP</option>
-      </select><br>
-      <label>SKill Used-3:</label>
-      <select name="noteP3">
-        <option value="0">None</option>
-        <option value="HTML5">HTML5</option>
-        <option value="CSS3">CSS3</option>
-        <option value="JavaScript">JavaScript</option>
-        <option value="jQuery">jQuery</option>
-        <option value="Ajax">Ajax</option>
-        <option value="PHP">PHP</option>
-      </select><br>
+        <select name="noteP1">
+          <option value="HTML5">HTML5</option>
+          <option value="CSS3">CSS3</option>
+          <option value="JavaScript">JavaScript</option>
+          <option value="jQuery">jQuery</option>
+          <option value="Ajax">Ajax</option>
+          <option value="PHP">PHP</option>
+        </select>
+      </div>
+      <div>
+        <label>SKill Used-2:</label>
+        <select name="noteP2">
+          <option value="0">None</option>
+          <option value="HTML5">HTML5</option>
+          <option value="CSS3">CSS3</option>
+          <option value="JavaScript">JavaScript</option>
+          <option value="jQuery">jQuery</option>
+          <option value="Ajax">Ajax</option>
+          <option value="PHP">PHP</option>
+        </select>
+      </div>
+      <div>
+        <label>SKill Used-3:</label>
+        <select name="noteP3">
+          <option value="0">None</option>
+          <option value="HTML5">HTML5</option>
+          <option value="CSS3">CSS3</option>
+          <option value="JavaScript">JavaScript</option>
+          <option value="jQuery">jQuery</option>
+          <option value="Ajax">Ajax</option>
+          <option value="PHP">PHP</option>
+        </select>
+      </div>
       <input type="submit" value="上傳">
     </form>
   </div>
